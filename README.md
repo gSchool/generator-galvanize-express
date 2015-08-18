cd3dc5d4f42f1f667b1dc00e543361af8c115a20
Michael Herman <hermanmu@gmail.com>
2015-08-17 19:40:21 -0600

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# generator-galvanize-express
+
+[Yeoman](http://yeoman.io) generator for a basic Node/Express boilerplate.
+
+## Quick Start
+
+1. Install Yeoman (if necessary) - `npm install -g yo`
+1. Install the generator - `npm install -g generator-galvanize-express`
+1. Run - `yo galvanize-express`
---
+++ app/index.js
@@ -0,0 +1,21 @@
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+
+var GalvanizeExpreessGenerator = yeoman.generators.Base.extend({
+
+  promptUser: function() {
+    // greeting
+    console.log(chalk.magenta("Welcome to Galvanize's Node/Express Generator"));
+  },
+
+  createApp: function(){
+    this.copy('client', 'client');
+    this.copy('package.json');
+    this.copy('.gitignore');
+  },
+
+});
+
+module.exports = GalvanizeExpreessGenerator;
