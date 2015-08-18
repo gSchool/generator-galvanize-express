34e6cfc8f83717f409d7763150c35ad8a3c99cac
Michael Herman <hermanmu@gmail.com>
2015-08-17 19:56:29 -0600

    bumped version

--- app/index.js
+++ app/index.js
@@ -11,8 +11,23 @@ var GalvanizeExpreessGenerator = yeoman.generators.Base.extend({
   },
 
   createApp: function(){
-    this.copy('client/*', 'client/*');
-    this.copy('server/*', 'server/*');
+    this.mkdir('client');
+    this.mkdir('client/public');
+    this.mkdir('client/public/css');
+    this.mkdir('client/public/img');
+    this.mkdir('client/public/js');
+    this.copy('client/public/css/main.css', 'client/public/css/main.js');
+    this.copy('client/public/css/main.css', 'client/public/css/main.js');
+    this.mkdir('server');
+    this.mkdir('server/bin');
+    this.mkdir('server/routes');
+    this.mkdir('server/views');
+    this.copy('server/bin/www', 'server/bin/www');
+    this.copy('server/routes/index.js', 'server/routes/index.js');
+    this.copy('server/views/error.html', 'server/views/error.html');
+    this.copy('server/views/index.html', 'server/views/index.html');
+    this.copy('server/views/layout.html', 'server/views/layout.html');
+    this.copy('server/app.js', 'server/app.js');
     this.copy('package.json');
     this.copy('.gitignore');
   },
