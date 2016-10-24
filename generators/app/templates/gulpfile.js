44b45d03e9e521cd37df2d7f7faae8f3e3f05678
benhassara <bhassara@gmail.com>
2016-10-24 11:36:01 -0600

    fix views gulp task

--- generators/app/templates/gulpfile.js
+++ generators/app/templates/gulpfile.js
@@ -92,7 +92,7 @@ gulp.task('nodemon', () => {
 });
 
 gulp.task('watch', () => {
-  gulp.watch(paths.html, ['html']);
+  gulp.watch(paths.views, ['views']);
   gulp.watch(paths.scripts, ['jshint', 'jscs']);
   gulp.watch(paths.styles, ['styles']);
 });
