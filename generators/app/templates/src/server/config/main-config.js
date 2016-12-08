eaff9109bc5ea716e1b7e57b997fcda746004074
benhassara <bhassara@gmail.com>
2016-12-08 12:45:43 -0700

    dont load .env in prod

--- generators/app/templates/src/server/config/main-config.js
+++ generators/app/templates/src/server/config/main-config.js
@@ -17,7 +17,9 @@
   ];
 
   // *** load environment variables *** //
-  require('dotenv').config();
+  if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+  }
 
   appConfig.init = function(app, express) {
 
